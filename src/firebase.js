2da5e09b116a7b034c09c2130995c287c7492ed5
divyansh <divyannshtiwari@ivolvelabs.com>
2025-06-27 11:55:01 +0530

    gemini first code assist working

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import { getFirestore} from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBLm4B4k9oLiNrq809eTyJRj0hZ15olk6Y",
-  authDomain: "aop-lab.firebaseapp.com",
-  projectId: "aop-lab",
-  storageBucket: "aop-lab.appspot.com",
-  messagingSenderId: "589069920397",
-  appId: "1:589069920397:web:4d7e0160e93145bf741ac4",
-  measurementId: "G-JJJ85ST0GS",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- src/index.js
+++ src/index.js
@@ -8,27 +8,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './Contexts/AuthContext';
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBLm4B4k9oLiNrq809eTyJRj0hZ15olk6Y",
-  authDomain: "aop-lab.firebaseapp.com",
-  projectId: "aop-lab",
-  storageBucket: "aop-lab.appspot.com",
-  messagingSenderId: "589069920397",
-  appId: "1:589069920397:web:4d7e0160e93145bf741ac4",
-  measurementId: "G-JJJ85ST0GS"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 
 const theme = createTheme({
